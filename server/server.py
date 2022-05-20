350ce73045fe9347ff980ca24219394604616d22
Amir Azhar <amir97azhar@gmail.com>
2022-05-21 00:57:09 +0800

    Update routes

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, request
 from bson.objectid import ObjectId
+from datetime import date
 import pymongo
 import json
 
@@ -11,22 +12,29 @@ try:
         host="localhost", port=27017, serverSelectionTimeoutMS=1000
     )
     mongo.server_info()  # trigger exception if cant connect to db
-    db = mongo.test
+    db = mongo.JobApplicationKanban
 except:
     print("ERROR: Could not connect to db")
 
 ################################################################
 
-# Create a single user
-@app.route("/users", methods=["POST"])
-def create_user():
+# Create a new job application
+@app.route("/api/applications", methods=["POST"])
+def createNewApplication():
     try:
-        user = {"name": request.form["name"], "email": request.form["email"]}
-        dbResponse = db.users.insert_one(user)
+        app = {
+            "company": request.form["company"],
+            "title": request.form["title"],
+            "link": request.form["link"],
+            "dateApplied": date.today().strftime("%d/%m/%Y"),
+            "dateUpdated": date.today().strftime("%d/%m/%Y"),
+            "status": "APPLIED",
+        }
+        dbResponse = db.applications.insert_one(app)
         return Response(
             response=json.dumps(
                 {
-                    "message": "User created successfully.",
+                    "message": "Application created successfully.",
                     "id": f"{dbResponse.inserted_id}",
                 }
             ),
@@ -37,13 +45,13 @@ def create_user():
         print(e)
 
 
-# Get all users
-@app.route("/users", methods=["GET"])
-def get_users():
+# Get all job applications
+@app.route("/api/applications", methods=["GET"])
+def getApplications():
     try:
-        data = list(db.users.find())
-        for user in data:
-            user["_id"] = str(user["_id"])
+        data = list(db.applications.find())
+        for app in data:
+            app["_id"] = str(app["_id"])
         return Response(
             response=json.dumps(data),
             status=200,
@@ -52,23 +60,29 @@ def get_users():
     except Exception as e:
         print(e)
         return Response(
-            response=json.dumps({"message": "Cannot retrieve users."}),
+            response=json.dumps({"message": "Cannot retrieve applications."}),
             status=500,
             mimetype="application/json",
         )
 
 
-# Update a single user
-@app.route("/users/<id>", methods=["PATCH"])
-def update_user(id):
+# Update a single job application
+@app.route("/api/applications/<id>", methods=["PATCH"])
+def updateApplication(id):
     try:
-        dbResponse = db.users.update_one(
-            {"_id": ObjectId(id)}, {"$set": {"name": request.form["name"]}}
+        dbResponse = db.applications.update_one(
+            {"_id": ObjectId(id)},
+            {
+                "$set": {
+                    "status": request.form["status"],
+                    "dateUpdated": date.today().strftime("%d/%m/%Y"),
+                }
+            },
         )
         return Response(
             response=json.dumps(
                 {
-                    "message": "User successfully updated.",
+                    "message": "Application successfully updated.",
                     "id": f"{id}",
                 }
             ),
@@ -78,21 +92,21 @@ def update_user(id):
     except Exception as e:
         print(e)
         return Response(
-            response=json.dumps({"message": "Cannot update user."}),
+            response=json.dumps({"message": "Cannot update application."}),
             status=500,
             mimetype="application/json",
         )
 
 
-# Delete a single user
-@app.route("/users/<id>", methods=["DELETE"])
-def delete_user(id):
+# Delete a job application
+@app.route("/api/applications/<id>", methods=["DELETE"])
+def deleteApplication(id):
     try:
-        dbResponse = db.users.delete_one({"_id": ObjectId(id)})
+        dbResponse = db.applications.delete_one({"_id": ObjectId(id)})
         return Response(
             response=json.dumps(
                 {
-                    "message": "User successfully deleted.",
+                    "message": "Application deleted successfully.",
                     "id": f"{id}",
                 }
             ),
@@ -102,7 +116,7 @@ def delete_user(id):
     except Exception as e:
         print(e)
         return Response(
-            response=json.dumps({"message": "Cannot delete user."}),
+            response=json.dumps({"message": "Cannot delete application."}),
             status=500,
             mimetype="application/json",
         )
