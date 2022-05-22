aeb37b9d08dfdfeddebe2bf834aec9f063ed5ea0
Amir Azhar <amir97azhar@gmail.com>
2022-05-23 00:07:51 +0800

    add sidebar template

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Form() {
-  return <div className="h-10 w-10 bg-gray-200">Form</div>;
+function Form({ setAddJob }) {
+  return (
+    <div class="absolute inset-y-0 right-0 w-80 h-screen bg-gray-300 shadow-md">
+      <button onClick={() => setAddJob(false)}>Test</button>
+    </div>
+  );
 }
 
 export default Form;
