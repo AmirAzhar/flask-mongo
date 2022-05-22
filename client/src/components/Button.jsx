5b311884bef6c7aacfd2a455b1f829984778910b
Amir Azhar <amir97azhar@gmail.com>
2022-05-23 02:40:52 +0800

    Add animation to sidebar

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -5,18 +5,18 @@ import { useState } from "react";
 import Form from "./Form";
 
 function Button() {
-  const [addJob, setAddJob] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div>
       <div className="absolute bottom-0 right-0">
         <button
-          onClick={() => setAddJob(true)}
-          className="flex justify-center items-center bg-blue-900 rounded-full h-10 w-10 m-4 hover:scale-125 transform ease-in-out duration-300"
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="flex justify-center items-center bg-blue-900 rounded-full h-14 w-14 m-4 hover:scale-125 transform ease-in-out duration-300"
         >
-          <span className="font-bold text-xl text-white">+</span>
+          <span className="font-bold text-2xl text-white">+</span>
         </button>
       </div>
-      {addJob && <Form setAddJob={setAddJob} />}
+      <Form setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
     </div>
   );
 }
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Form({ setAddJob }) {
+function Form({ showSidebar, setShowSidebar }) {
   return (
-    <div class="absolute inset-y-0 right-0 w-80 h-screen bg-gray-300 shadow-md">
-      <button onClick={() => setAddJob(false)}>Test</button>
+    <div
+      className={`transform top-0 right-0 w-60 bg-gray-200 fixed h-full ease-in-out duration-300 ${
+        showSidebar ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <button onClick={() => setShowSidebar(!showSidebar)}>Test</button>
     </div>
   );
 }
